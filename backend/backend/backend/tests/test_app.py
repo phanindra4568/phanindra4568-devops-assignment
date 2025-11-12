d46c2480a85a7188169fb64b6e519bd2a2b40f65
phanindra4568 <137140900+phanindra4568@users.noreply.github.com>
2025-11-12 14:09:48 +0530

    Create test_app.py

---
+++ backend/backend/backend/tests/test_app.py
@@ -0,0 +1,11 @@
+from app import app
+
+def test_health():
+    client = app.test_client()
+    res = client.get("/health")
+    assert res.status_code == 200
+
+def test_message():
+    client = app.test_client()
+    res = client.get("/api/message")
+    assert "message" in res.json
